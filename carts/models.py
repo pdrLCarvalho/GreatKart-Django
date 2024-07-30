10ddfa06333c01e616c73b984318ce3df614d355
Pedro <pedrolcrg@gmail.com>
2024-07-30 10:13:05 -0300

    alteracoes na pagina cart

--- carts/models.py
+++ carts/models.py
@@ -16,5 +16,8 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
+    def sub_total(self):
+        return self.product.price * self.quantity
+
+    def __unicode__(self):
         return self.product
