1f035867a95227b6d04b6496be1a4a462accc29e
Pedro <pedrolcrg@gmail.com>
2024-09-03 16:35:34 -0300

    Verificação de dois fatores para postar uma avaliação , e adicionando as funcionalidades de avaliação média e contagem de avaliações

--- store/views.py
+++ store/views.py
@@ -9,6 +9,7 @@ from django.db.models import Q
 
 from .forms import ReviewForm
 from django.contrib import messages
+from orders.models import OrderProduct
 
 
 
@@ -48,9 +49,22 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    if request.user.is_authenticated:
+        try:
+            orderproduct =OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+
+    #Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status= True)
+        
     context ={
         'single_product': single_product,
         'in_cart'       : in_cart,
+        'orderproduct'  : orderproduct,
+        'reviews'       : reviews,
     }
 
     return render(request, 'store/product_detail.html',context)
